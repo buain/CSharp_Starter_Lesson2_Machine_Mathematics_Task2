73811a7f0d12297523dfd3b9a92aeead55b762b7
Vladimir <33662107+buain@users.noreply.github.com>
2018-01-26 17:14:51 +0300

    Initial commit

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double pi = 3.141592653;
+            Console.WriteLine(pi);
+           Decimal e = 2.7182818284590452M;
+            Console.WriteLine(e);
+            Console.ReadLine();
+        }
+    }
+}
